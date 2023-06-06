cc08d0f2d7f955f82fbd4e00a64e455de7d98bd2
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-06-06 13:47:38 +0300

    🎨 style(RegistrationPage.tsx): refactor RegistrationPage component to use formik for form handling and add validation schema to form fields

--- src/pages/RegistrationPage.tsx
+++ src/pages/RegistrationPage.tsx
@@ -1,55 +1,86 @@
-import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import React from 'react';
+import { Form, Button, Container } from 'react-bootstrap';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { CreateUserDto } from '../dtos/users.dto';
 
 const RegistrationPage = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
-    const handleRegistration = (e: React.FormEvent) => {
-        e.preventDefault();
-        // Handle registration logic here
+    const initialValues: CreateUserDto = {
+        name: '',
+        email: '',
+        password: ''
     };
+    const validationSchema = Yup.object<CreateUserDto>({
+        name: Yup.string().required('Name is required'),
+        email: Yup.string().email('Invalid email address').required('Email is required'),
+        password: Yup.string().required('Password is required')
+    });
+
+    const formik = useFormik({
+        initialValues,
+        validationSchema,
+        onSubmit: (values) => {
+            // Handle registration logic here
+            console.log(values);
+        }
+    });
+
+    const {
+        handleSubmit,
+        handleChange,
+        values,
+        touched,
+        errors
+    } = formik;
 
     return (
-        <div>
+        <Container>
             <h1>Registration</h1>
-            <Form onSubmit={handleRegistration}>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group controlId="name">
                     <Form.Label>Name</Form.Label>
                     <Form.Control
                         type="text"
+                        name="name"
                         placeholder="Enter your name"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
+                        value={values.name}
+                        onChange={handleChange}
+                        isInvalid={!!touched.name && !!errors.name}
                     />
+                    <Form.Control.Feedback type="invalid">{errors.name}</Form.Control.Feedback>
                 </Form.Group>
 
                 <Form.Group controlId="email">
-                    <Form.Label>Email address</Form.Label>
+                    <Form.Label>Email</Form.Label>
                     <Form.Control
                         type="email"
+                        name="email"
                         placeholder="Enter email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
+                        value={values.email}
+                        onChange={handleChange}
+                        isInvalid={!!touched.email && !!errors.email}
                     />
+                    <Form.Control.Feedback type="invalid">{errors.email}</Form.Control.Feedback>
                 </Form.Group>
 
                 <Form.Group controlId="password">
                     <Form.Label>Password</Form.Label>
                     <Form.Control
                         type="password"
+                        name="password"
                         placeholder="Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
+                        value={values.password}
+                        onChange={handleChange}
+                        isInvalid={!!touched.password && !!errors.password}
                     />
+                    <Form.Control.Feedback type="invalid">{errors.password}</Form.Control.Feedback>
                 </Form.Group>
-
+                <br />
                 <Button variant="primary" type="submit">
                     Register
                 </Button>
             </Form>
-        </div>
+        </Container>
     );
 };
 
