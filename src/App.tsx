4a5a46d56aacb2a27461f6fec34665bc80d7afae
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-06-06 21:31:27 +0300

    🔒 chore(App.tsx): add PrivateRoute component to handle authenticated routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import RegistrationPage from './pages/RegistrationPage';
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
+import { Context, Provider } from './context';
+
+function PrivateRoute({ element, path }: { element: React.JSX.Element; path: string }) {
+    const { isAuth } = useContext(Context);
+
+    if (!isAuth) {
+        return <Navigate to={path} />;
+    }
+
+    return element;
+}
 
 const App: React.FC = () => {
-    const isAuthenticated = false;
-
     return (
-        <Router>
-            <Routes>
-                {isAuthenticated ? (
-                    <>
-                        <Route path="/" element={<Navigate to="/home" />} />
-                        <Route path="/home" element={<HomePage />} />
-                    </>
-                ) : (
-                    <>
-                        <Route path="/" element={<Navigate to="/login" />} />
-                        <Route path="/register" element={<RegistrationPage />} />
-                        <Route path="/login" element={<LoginPage />} />
-                    </>
-                )}
-                <Route path="*" element={<Navigate to="/" />} />
-            </Routes>
-        </Router>
+        <Provider>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Navigate to="/home" />} />
+                    <Route path="/home" element={<PrivateRoute path="/login" element={<HomePage />} />} />
+                    <Route path="/register" element={<RegistrationPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="*" element={<Navigate to="/" />} />
+                </Routes>
+            </Router>
+        </Provider>
     );
 };
 
