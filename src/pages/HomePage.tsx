7da182c3f49207bb88414df4d96acfc52937832d
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-06-06 22:39:12 +0300

    ✨ feat(package.json, HomePage.tsx): add luxon and react-bootstrap-table-next dependencies, add BootstrapTable component to display users data with columns, add logout button

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,71 +1,99 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Table, Button, Spinner } from 'react-bootstrap';
+import { Button, Container, Spinner } from 'react-bootstrap';
+import BootstrapTable from 'react-bootstrap-table-next';
+import { useNavigate } from 'react-router-dom';
 import { getUsers } from '../services/users.service';
 import { logOut } from '../services/auth.service';
 import { Context } from '../context';
+import { User } from '../interfaces/users.interface';
+import { Status } from '../constants';
 
 const HomePage: React.FC = () => {
+    const navigate = useNavigate();
     const { getUser, logout } = useContext(Context);
-    const [users, setUsers] = useState<any>([]);
-    const [loading, setLoading] = useState(false);
+    const [users, setUsers] = useState<User[]>([]);
+    const [status, setStatus] = useState(Status.Idle);
+    const [logoutStatus, setLogoutStatus] = useState(Status.Idle);
+    const [message, setMessage] = useState('');
 
     useEffect(() => {
-        const fetchUsers = async () => {
-            try {
-                const response = await getUsers();
-                setUsers(response.data.data);
-            } catch (error) {
-                console.error('Error fetching users:', error);
-            }
-        };
-
-        fetchUsers();
+        setStatus(Status.Loading);
+        getUsers().then(response => {
+            setStatus(Status.Succeeded);
+            setUsers(response.data.data);
+        }).catch(error => {
+            setStatus(Status.Failed);
+            setMessage(error.message);
+        });
     }, []);
 
     const handleLogout = () => {
-        try {
-            setLoading(true);
-            const user = getUser();
-
-            user && logOut(user).then(logout);
-        } catch (error) {
-            console.error('Error logging out:', error);
-        } finally {
-            setLoading(false);
-        }
+        setLogoutStatus(Status.Loading);
+        const user = getUser();
+        user && logOut(user).then(logout)
+            .then(() => {
+                setLogoutStatus(Status.Succeeded);
+            })
+            .catch(() => {
+                setLogoutStatus(Status.Failed);
+            }).finally(() => {
+                navigate('/login');
+            });
     };
 
+    const handleSelect = (row: any, isSelected: boolean, rowIndex: number) => {
+        console.log('@@@', { row, isSelected, rowIndex });
+    };
+
+    const handleSelectAll = (isSelect: boolean, rows: any, e: React.SyntheticEvent<Element, Event>) => {
+        console.log('@@@', { isSelect, rows, e });
+    };
+
+    const columns = [{
+        dataField: 'id',
+        text: 'Id'
+    }, {
+        dataField: 'name',
+        text: 'Name'
+    }, {
+        dataField: 'email',
+        text: 'Email'
+    }, {
+        dataField: 'registrationDate',
+        text: 'Registration Date'
+    }, {
+        dataField: 'lastLoginDate',
+        text: 'Last Login Date'
+    }, {
+        dataField: 'status',
+        text: 'Status'
+    }];
+
     return (
-        <div>
-            <Button variant="danger" onClick={handleLogout} disabled={loading}>
-                {loading ? <Spinner animation="border" size="sm" /> : 'Logout'}
-            </Button>
+        <Container>
             <h1>Users</h1>
-            <Table striped bordered>
-                <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Registration Date</th>
-                    <th>Last Login Date</th>
-                    <th>Status</th>
-                </tr>
-                </thead>
-                <tbody>
-                {users.map((user: any) => (
-                    <tr key={user.id}>
-                        <td>{user.id}</td>
-                        <td>{user.name}</td>
-                        <td>{user.email}</td>
-                        <td>{user.registrationDate}</td>
-                        <td>{user.lastLoginDate}</td>
-                        <td>{user.status}</td>
-                    </tr>
-                ))}
-                </tbody>
-            </Table>
-        </div>
+            <BootstrapTable
+                bootstrap4
+                keyField="id"
+                data={users}
+                columns={columns}
+                caption={(
+                    <Button
+                        variant="danger"
+                        onClick={handleLogout}
+                        disabled={logoutStatus === Status.Loading || logoutStatus === Status.Succeeded}
+                    >
+                        {logoutStatus === Status.Loading ? <Spinner animation="border" size="sm" /> : 'Logout'}
+                    </Button>
+                )}
+                selectRow={{
+                    mode: 'checkbox',
+                    clickToSelect: true,
+                    onSelect: handleSelect,
+                    onSelectAll: handleSelectAll
+                }}
+            />
+        </Container>
     );
 };
 
