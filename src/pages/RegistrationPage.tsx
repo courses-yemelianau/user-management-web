7a90d79320decfe66b7d5b6f10fc3d2615d97af3
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-06-06 15:06:21 +0300

    ✨ feat(RegistrationPage.tsx): add form submission logic to handle user registration and display success/error messages

--- src/pages/RegistrationPage.tsx
+++ src/pages/RegistrationPage.tsx
@@ -1,27 +1,64 @@
-import React from 'react';
-import { Form, Button, Container } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Alert, Button, Container, Form, Spinner } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { CreateUserDto } from '../dtos/users.dto';
+import { signUp } from '../services/auth.service';
+import { Status } from '../constants';
 
 const RegistrationPage = () => {
+    const navigate = useNavigate();
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [showErrorMessage, setShowErrorMessage] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+    const [status, setStatus] = useState(Status.Idle);
+
     const initialValues: CreateUserDto = {
         name: '',
         email: '',
         password: ''
     };
+
     const validationSchema = Yup.object<CreateUserDto>({
-        name: Yup.string().required('Name is required'),
-        email: Yup.string().email('Invalid email address').required('Email is required'),
-        password: Yup.string().required('Password is required')
+        name: Yup
+            .string()
+            .required('Name is required'),
+        email: Yup
+            .string()
+            .email('Invalid email address')
+            .required('Email is required')
+            .matches(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Invalid email address'),
+        password: Yup
+            .string()
+            .required('Password is required')
     });
 
     const formik = useFormik({
         initialValues,
         validationSchema,
         onSubmit: (values) => {
-            // Handle registration logic here
-            console.log(values);
+            setStatus(Status.Loading);
+            setShowErrorMessage(false);
+            signUp(values)
+                .then(() => {
+                    setStatus(Status.Succeeded);
+                    setShowSuccessMessage(true);
+                    setTimeout(() => {
+                        setShowSuccessMessage(false);
+                        navigate('/login');
+                    }, 3000);
+                })
+                .catch((error) => {
+                    setStatus(Status.Failed);
+                    if (error.response && error.response.status === 409) {
+                        setShowErrorMessage(true);
+                        setErrorMessage(`This email ${values.email} already exists`);
+                    } else {
+                        setShowErrorMessage(true);
+                        setErrorMessage('An error occurred. Please try again.');
+                    }
+                });
         }
     });
 
@@ -36,6 +73,16 @@ const RegistrationPage = () => {
     return (
         <Container>
             <h1>Registration</h1>
+            {showSuccessMessage && (
+                <Alert variant="success" onClose={() => setShowSuccessMessage(false)} dismissible>
+                    Registration successful! Redirecting to login page...
+                </Alert>
+            )}
+            {showErrorMessage && (
+                <Alert variant="danger" onClose={() => setShowErrorMessage(false)} dismissible>
+                    {errorMessage}
+                </Alert>
+            )}
             <Form onSubmit={handleSubmit}>
                 <Form.Group controlId="name">
                     <Form.Label>Name</Form.Label>
@@ -76,8 +123,12 @@ const RegistrationPage = () => {
                     <Form.Control.Feedback type="invalid">{errors.password}</Form.Control.Feedback>
                 </Form.Group>
                 <br />
-                <Button variant="primary" type="submit">
-                    Register
+                <Button
+                    variant="primary"
+                    type="submit"
+                    disabled={status === Status.Loading || status === Status.Succeeded}
+                >
+                    {status === Status.Loading ? <Spinner animation="border" size="sm" /> : 'Register'}
                 </Button>
             </Form>
         </Container>
