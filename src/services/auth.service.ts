4b55bf6725fbd9459cbd7f58c3d70ee8c1c00f56
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-06-07 12:56:57 +0300

    🔨 refactor(auth.service.ts, users.service.ts): change function names to improve semantics and consistency

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -4,13 +4,13 @@ import { Response } from '../interfaces/response.interface';
 import { User } from '../interfaces/users.interface';
 
 export const signUp = (userData: CreateUserDto) => {
-    return axios.post<Response<User>>(`/signup`, userData);
+    return axios.post<Response<User>>('/signup', userData);
 };
 
 export const logIn = (userData: LoginUserDto) => {
-    return axios.post<Response<User>>(`/login`, userData, { withCredentials: true });
+    return axios.post<Response<User>>('/login', userData, { withCredentials: true });
 };
 
 export const logOut = (userData: User) => {
-    return axios.post<Response<User>>(`/logout`, userData, { withCredentials: true });
+    return axios.post<Response<User>>('/logout', userData, { withCredentials: true });
 };
--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -9,10 +9,10 @@ export const getUsers = () => {
     return axios.get<Response<User[]>>(`${path}`, { withCredentials: true });
 };
 
-export const updateUser = (userId: number, userData: UpdateUserDto) => {
-    return axios.put<Response<User>>(`${path}/${userId}`, userData, { withCredentials: true });
+export const updateUsers = (userIds: number[], userData: UpdateUserDto) => {
+    return axios.put<Response<User[]>>(`${path}/update`, { userIds, userData }, { withCredentials: true });
 };
 
-export const deleteUser = (userId: number) => {
-    return axios.delete<Response<User>>(`${path}/${userId}`, { withCredentials: true });
+export const deleteUsers = (userIds: number[]) => {
+    return axios.delete<Response<User[]>>(`${path}/delete`, { data: { userIds }, withCredentials: true });
 };
